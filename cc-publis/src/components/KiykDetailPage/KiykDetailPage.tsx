c3a8fb032d36ea17d226ff620152afcf94c28e15
miyamoto <miyamoto.chichi@gmail.com>
2020-02-29 10:53:06 +0900

    add KiykTankaZandakaContainer

--- cc-publis/src/components/KiykDetailPage/KiykDetailPage.tsx
+++ cc-publis/src/components/KiykDetailPage/KiykDetailPage.tsx
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 import { useHistory } from "react-router-dom";
 
 import KiykDetailContainer from "./kiykDetail/KiykDetailContainer";
+import KiykTankaZandakaContainer from "./kiykTankaZandaka/KiykTankaZandakaContainer";
 import KiykZandakaHistoryContainer from "./kiykZandakaHistory/KiykZandakaHistoryContainer";
 
 import "./KiykDetailPage.css";
@@ -22,6 +23,7 @@ const KiykDetailPage: React.FC = () => {
         <button onClick={() => history.push("/cstm")}>顧客マスタ</button>
       </div>
       <KiykDetailContainer />
+      <KiykTankaZandakaContainer />
       <KiykZandakaHistoryContainer />
     </div>
   );
